0838243d58e89d8e894d2b00760138860174e158
gallaugher <john.gallaugher@bc.edu>
2026-01-18 17:05:03 -0500

    Initial Commit

---
+++ DungeonDice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DungeonDice
+//  Created by John Gallaugher on 1/18/26.
+//  YouTube.com/profgallaugher - gallaugher.bsky.social
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//  Created by John Gallaugher on 1/18/26.
+//  YouTube.com/profgallaugher - gallaugher.bsky.social
+
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
